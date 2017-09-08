51cfa845eb337448bec08a98e2e932fb28cd914c
Amitai Gat <amitaigat@gmail.com>
2017-09-08 11:53:20 +0300

    sample

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// TODO: enabled when ready -> registerServiceWorker();
