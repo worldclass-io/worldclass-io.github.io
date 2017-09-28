27ea713ca222e50304f691ebf7ccdb63eac24942
Amitai Gat <amitaigat@gmail.com>
2017-09-28 17:46:31 +0300

    payment

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ class App extends Component {
               <hr className="w-200 my-90" />
               <div className="row gap-y align-items-center">
                 <div className="col-12 col-md-5 text-center">
-                  <img src={require('./img/credit-card.png')} alt="..." />
+                  <img src={require('./img/credit-card.png')} style={{height: 250}} alt="..." />
                 </div>
                 <div className="col-12 offset-md-1 col-md-6">
                   <h5>Instant Payouts, No Comissions!</h5>
